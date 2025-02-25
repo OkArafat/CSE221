3f016d7b634857fa1843fb3ebe43e0316feadf54
OkArafat <arafatbiswas.edu01@gmail.com>
2025-02-25 16:06:47 +0600

    addtask-2

---
+++ CSE221 Lab-01 Spring 2025/task-2.py
@@ -0,0 +1,23 @@
+import sys
+import operator as op
+
+calculation={"+":op.add, "-":op.sub, "*":op.mul, "/":op.truediv}
+
+t=int(sys.stdin.readline().strip())
+for i in range(t):
+    i,num1, op, num2 = sys.stdin.readline().strip().split()
+    num1 = int(num1)
+    num2 = int(num2)
+    result = calculation[op](num1,num2)
+    print(f"{result:.6f}")
+    
+
+
+
+
+
+
+
+    
+   
+  
